0e38e535822ba51ae3eb6062f651692fe75a42f8
Ben Goodwyn <bengoodwyn@me.com>
2022-01-08 13:43:50 -0800

    :art: Use ericniebler/range-v3 for more declarative code

--- lib/aoc2021/include/aoc2021/day1/count_increases_over_window.hpp
+++ lib/aoc2021/include/aoc2021/day1/count_increases_over_window.hpp
@@ -1,15 +1,26 @@
 #pragma once
 
+#include <cstddef>
+#include <range/v3/algorithm/count_if.hpp>
+#include <range/v3/view/drop.hpp>
+#include <range/v3/view/filter.hpp>
+#include <range/v3/view/zip.hpp>
+
 namespace aoc2021::day1 {
-template <size_t window_size>
+
+template <std::size_t window_size>
 constexpr auto count_increases_over_window(const auto &values) {
-  size_t count{};
-  for (auto it = std::cbegin(values); it != (std::cend(values) - window_size);
-       ++it) {
-    if (*(it + window_size) > *it) {
-      ++count;
-    }
-  }
-  return count;
+  using ranges::views::zip, ranges::views::drop, ranges::count_if;
+  using std::get;
+
+  const auto values_before_and_after_window =
+      zip(values, values | drop(window_size));
+
+  constexpr auto after_is_larger = [](const auto &x) {
+    return get<1>(x) > get<0>(x);
+  };
+
+  return count_if(values_before_and_after_window, after_is_larger);
 };
+
 } // namespace aoc2021::day1
